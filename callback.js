b72181816e45607a63f20571c94ef1e65b2c70e4
Roman <Romanklacko5138@gmail.com>
2021-03-31 21:30:22 +0300

    callback

--- callback.js
+++ callback.js
@@ -7,42 +7,26 @@
 
 // highOrder('name surname');
 
-function question(job) {
-  const jobsDictionary = {
-    developer: 'Что такое JS?',
-    teacher: 'Какой предмет вы преподаете?',
-  };
+// function question(job) {
+//   const jobsDictionary = {
+//     developer: "Что такое JS?",
+//     teacher: "Какой предмет вы преподаете?",
+//   };
 
-  return function(name) {
-    return jobsDictionary[job] + ' ' + name;
-  };
-}
+//   return function (name) {
+//     return jobsDictionary[job] + " " + name;
+//   };
+// }
 
-const developerQustion = question('developer');
-console.log(developerQustion);
-console.log(developerQustion('name'));
+// const developerQustion = question("developer");
+// console.log(developerQustion);
+// console.log(developerQustion("name"));
 
-// Задачи 
+// Задачи
 
-function high_func(list, handler){
-let res = list.forEach(element => handler(element)
-);
-return `new value: ${res}`
-}
-
-function handler_1(element){
-let res += element
-return res
-}
-
-
-
-
-
-// 1. Создать две функции и дать им осмысленные названия:
+// 1. Создать две функции:
 // - первая функция принимает массив и колбэк (одна для всех вызовов)
 // - вторая функция (колбэк) обрабатывает каждый элемент массива (для каждого вызова свой callback)
-
 // Первая функция возвращает строку “New value: ” и результат обработки:
 
 // firstFunc([‘my’, ‘name’, ‘is’, ‘Trinity’], handler1) → “New value: MyNameIsTrinity”
@@ -51,22 +35,56 @@ return res
 // “New value: Jhon is 45, Aaron is 20,”
 // firstFunc([‘abc’, ‘123’], handler4) → “New value: cba, 321,” // строки инвертируются
 
+// function high_func(list, handler) {
+//   list = list.map((element) => (element = handler(element)));
+//   return `New value: ${list.join("")}`;
+// }
+// function handler_1(element) {
+//   return element[0].toUpperCase() + element.slice(1);
+// }
+// function handler_2(element) {
+//   return element * 10 + ", ";
+// }
+// function handler_3(element) {
+//   return `${element.name} is ${element.age}, `;
+// }
 
-// Подсказка: secondFunc должна быть представлена функцией, которая принимает
-// один аргумент (каждый элемент массива) и возвращает результат его обработки
+// function handler_4(element) {
+//   element = element.split("").reverse().join("");
+//   return `${element}, `;
+// }
+// console.log(high_func(["my", "name", "is", "Trinity"], handler_1));
+// console.log(high_func([10, 20, 30], handler_2));
+// console.log(
+//   high_func(
+//     [
+//       { age: 45, name: "Jhon" },
+//       { age: 20, name: "Aaron" },
+//     ],
+//     handler_3
+//   )
+// );
+// console.log(high_func(["abc", "123"], handler_4));
 
-
-
-
-
-
-// 2. Написать аналог метода every. Создайте функцию every, 
-// она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив) 
+// 2. Написать аналог метода every. Создайте функцию every,
+// она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив)
 // вторым аргументом callback (обязательно проверьте что передана функция)
-// функция должна возвращать true или false в зависимости от результата вызова callback 
-// (проверить число больше 5). Callback  должен принимать один элемент массива, его индекс в массиве и весь массив. 
- 
+// функция должна возвращать true или false в зависимости от результата вызова callback
+// (проверить число больше 5). Callback  должен принимать один элемент массива, его индекс в массиве и весь массив.
 
+// function my_every(list, handler) {
+//   if (!Array.isArray(list) || typeof handler !== "function") {
+//     console.log("no list or function given");
+//   }
+//   let res;
+//   for (let index = 0; index < list.length; index++) {
+//     res = handler(list[index], index, list);
+//     if (!res) return res;
+//   }
+//   return res;
+// }
 
-
-
+// function handler(element, index, list) {
+//   return element > 5;
+// }
+// console.log(my_every([12, 14, 29, 22, 26], handler));
